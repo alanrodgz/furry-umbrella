d95fc0427013c966d2a3ee61381607aa802978f4
Alan Rodriguez <40346132+alanrodgz@users.noreply.github.com>
2021-08-26 16:38:59 -0700

    Add files via upload
    solution

--- spiralOrder.java
+++ spiralOrder.java
@@ -5,28 +5,29 @@ class Solution {
         int columns = matrix[0].length;
         int up = 0;
         int left = 0;
-        int right = columns-1;
-        int down = rows-1;
-        
-        while(result.size() < rows * columns){
-            // Left to right
-            for(int col = left; col <= right; col++)
+        int right = columns - 1;
+        int down = rows - 1;
+
+        while (result.size() < rows * columns) {
+            // Traverse from left to right.
+            for (int col = left; col <= right; col++) {
                 result.add(matrix[up][col]);
-            
-            // Downward
-            for(int row = up+1; row <= down; row++)
+            }
+            // Traverse downwards.
+            for (int row = up + 1; row <= down; row++) {
                 result.add(matrix[row][right]);
-            
-            // Right to left
-            if(up != down){
-                for(int col = right-1; col >= left; col--){
+            }
+            // Make sure we are now on a different row.
+            if (up != down) {
+                // Traverse from right to left.
+                for (int col = right - 1; col >= left; col--) {
                     result.add(matrix[down][col]);
                 }
             }
-            
-            // Upward
-            if(left != right){
-                for(int row = down-1; row >= up; row--){
+            // Make sure we are now on a different column.
+            if (left != right) {
+                // Traverse upwards.
+                for (int row = down - 1; row > up; row--) {
                     result.add(matrix[row][left]);
                 }
             }
@@ -35,7 +36,7 @@ class Solution {
             up++;
             down--;
         }
-        
+
         return result;
     }
 }
